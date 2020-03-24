ec74a09facd09c78955d3b00be9fcce6b786cfdd
Sthyao <qq1171098650@163.com>
2020-03-24 14:31:22 +0800

    no message

--- risingstar.py
+++ risingstar.py
@@ -9,16 +9,22 @@ n_two = total - five*20 - four*4 - three - two
 
 if n_two >= 0:
     n_n = n_two * 3 + two * 2
-    n_w = 25 - five*5 - four
-    print("You still need lv1 N cards:",n_n)
-    print("You still need lv20 N cards:",n_two)
-    print("You still need 4※ white eges:",n_w)
+    n_w = 30 - five*5 - four
+    if n_n < 0 or n_w < 0:
+        print("Error")
+    else:
+        print("You still need lv1 N cards:",n_n)
+        print("You still need lv20 N cards:",n_two)
+        print("You still need 4※ white eges:",n_w)
 else:
     n_n = (n_two + two) * 2
-    n_w = 25 - five*5 - four
-    print("You have adequate lv.20 N cards, but:")
-    print("You still need lv1 N cards:",n_n)
-    print("You still need 4※ white eges:",n_w)
+    n_w = 30 - five*5 - four
+     if n_n < 0 or n_w < 0:
+        print("Error")
+    else:
+        print("You have adequate lv.20 N cards, but:")
+        print("You still need lv1 N cards:",n_n)
+        print("You still need 4※ white eges:",n_w)
 
 
 
