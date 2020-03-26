3621f8cdccbcf3b158888031a024ead0da3155f5
Sthyao <qq1171098650@163.com>
2020-03-26 22:38:15 +0800

    no message

--- risingstar.py
+++ risingstar.py
@@ -1,30 +1,36 @@
-five = int(input("how many 5※ do you have:"))
-four = int(input("how many 4※ do you have:"))
-three = int(input("how many 3※ do you have:"))
-two = int(input("how many 2※ but mix leavl do you have:"))
+try:
+    five = int(input("how many 5※ do you have:"))
+except:
+    five = 0
+try:
+    four = int(input("how many 4※ do you have:"))
+except:
+    four = 0
+try: 
+    three = int(input("how many 3※ do you have:"))
+except:
+    three = 0
+try:
+    two = int(input("how many 2※ but mix leavl do you have:"))
+except:
+    two = 0
 
-total = 6*5*4 - 26
+total = 6*5*4 - 24
 #white eges and ssr been excluded
-n_two = total - five*20 - four*4 - three - two
+n_two = total - five*16 - four*3 - three - two
+n_n = n_two * 3 + two * 2
+n_w = 30 - five*5 - four
 
-if n_two >= 0:
-    n_n = n_two * 3 + two * 2
-    n_w = 30 - five*5 - four
-    if n_n < 0 or n_w < 0:
-        print("Error")
-    else:
-        print("You still need lv1 N cards:",n_n)
-        print("You still need lv20 N cards:",n_two)
-        print("You still need 4※ white eges:",n_w)
+
+if n_n < 0 or n_w < 0:
+    print("Error")
 else:
-    n_n = (n_two + two) * 2
-    n_w = 30 - five*5 - four
-     if n_n < 0 or n_w < 0:
-        print("Error")
-    else:
-        print("You have adequate lv.20 N cards, but:")
+    if n_two >= 0:
         print("You still need lv1 N cards:",n_n)
-        print("You still need 4※ white eges:",n_w)
+    else:
+        print("You just need rise 2※:",two+n_two)
+    print("You still need lv20 N cards:",n_two)
+    print("You still need 4※ white eges:",n_w)
 
 
 
